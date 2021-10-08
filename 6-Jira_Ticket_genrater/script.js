be7ffbab6486ced3709f4ecba7e884dc49552d76
Prakash Ranjan Rai <56366662+prakash912@users.noreply.github.com>
2021-10-08 22:40:35 +0530

    Update script.js

--- 6-Jira_Ticket_genrater/script.js
+++ 6-Jira_Ticket_genrater/script.js
@@ -1,45 +1,37 @@
+//variables
 var uid=new ShortUniqueId();
-let input=document.querySelector(".task_input");
-let mainContainer=document.querySelector(".main-container");
 let colors=["pink","blue","green","black"];
 let defaultColor="black";
 let cFilter="";
+let locked=false;
+let deleteMode=false;
+
+
+//elements
+let input=document.querySelector(".task_input");
+let mainContainer=document.querySelector(".main-container");
+let colorContainer=document.querySelector(".color-group_container");
+let lockContainer = document.querySelector(".lock-container");
+let unlockContainer = document.querySelector(".unlock-container");
+let plusContainer = document.querySelector(".plus-container");
+
+let deleteContainer = document.querySelector(".multiply-container");
+
+
+//event listner
 input.addEventListener("keydown",function(e){
     if(e.code=="Enter" &&input.value){
         console.log("task value",input.value);
         let id=uid();
-        createTask(id,input.value);
+        createTask(id,input.value,true);
         input.value="";
     }
 })
 
-function createTask(id,task){
-    let taskContainer=document.createElement("div");
-    taskContainer.setAttribute("class","task_container");
-    mainContainer.appendChild(taskContainer);
-    taskContainer.innerHTML=`<div class="task_header ${defaultColor}"></div>
-    <div class="task_main_container">
-       <h3 class="task_id">#${id}</h3>
-       <div class="text" contentEditable="true">${task}</div>
-</div>`;
 
-
-
-// add event listner for color change
-
-let taskHeader=taskContainer.querySelector(".task_header");
-taskHeader.addEventListener("click",function(){
-    let cColor=taskHeader.classList[1];
-    let idx=colors.indexOf(cColor);
-    let nextIdx=(idx+1)%4;
-    let nextColor=colors[nextIdx];
-    taskHeader.classList.remove(cColor);
-    taskHeader.classList.add(nextColor);
-})
-}
 //filtering
 
-let colorContainer=document.querySelector(".color-group_container");
+
 colorContainer.addEventListener("click",function(e){
     let element=e.target;
     console.log("e.target",element);
@@ -48,6 +40,88 @@ colorContainer.addEventListener("click",function(e){
         filterCards(filteredCardColor);
     }
 })
+
+
+lockContainer.addEventListener("click", function (e) {
+    let numberOFElements = document.querySelectorAll(".task_main-container>div")
+    for (let i = 0; i < numberOFElements.length; i++) {
+        numberOFElements[i].contentEditable = false;
+    }
+    // ui match
+    lockContainer.classList.add("active");
+    unlockContainer.classList.remove("active");
+})
+unlockContainer.addEventListener("click", function (e) {
+  let  numberOFElements = document.querySelectorAll(".task_main-container>div")
+    for (let i = 0; i < numberOFElements.length; i++) {
+        numberOFElements[i].contentEditable = true;
+    }
+    lockContainer.classList.remove("active");
+    unlockContainer.classList.add("active");
+})
+
+deleteContainer.addEventListener("click", function (e) {
+    deleteMode = !deleteMode;
+    if (deleteMode) {
+        deleteContainer.classList.add("active")
+    } else {
+        deleteContainer.classList.remove("active")
+
+    }
+})
+
+
+
+function createTask(id,task,flag){
+    let taskContainer=document.createElement("div");
+    taskContainer.setAttribute("class","task_container");
+    mainContainer.appendChild(taskContainer);
+    taskContainer.innerHTML=`<div class="task_header
+     ${defaultColor}"></div>
+    <div class="task_main-container">
+       <h3 class="task_id">#${id}</h3>
+       <div class="text" contentEditable="true">${task}</div>
+</div>
+`;
+
+
+
+// add event listner for color change
+
+let taskHeader=taskContainer.querySelector(".task_header");
+let nextColor;
+taskHeader.addEventListener("click",function(){
+    let cColor=taskHeader.classList[1];
+    let idx=colors.indexOf(cColor);
+    let nextIdx=(idx+1)%4;
+    let nextColor=colors[nextIdx];
+    taskHeader.classList.remove(cColor);
+    taskHeader.classList.add(nextColor);
+
+})
+
+taskContainer.addEventListener("click", function (e) {
+    if (deleteMode == true) {
+        // delete ->ui , storage
+
+        taskContainer.remove();
+
+    }
+})
+
+if (flag == true) {
+    let tasksString = localStorage.getItem("tasks");
+    let tasksArr = JSON.parse(tasksString) || [];
+    let taskObject = {
+        id: id,
+        task: task,
+        color: defaultColor
+    }
+    tasksArr.push(taskObject);
+    localStorage.setItem("tasks", JSON.stringify(tasksArr));
+}
+}
+
 
 
 
@@ -71,3 +145,14 @@ function filterCards(filterColor){
         }
     }
 }
+
+
+(function () {
+    // localStorage
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    for (let i = 0; i < tasks.length; i++) {
+        let { id, task, color } = tasks[i];
+        createTask(id,task,false);
+    }
+    // get it to ui
+})();
